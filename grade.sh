c565a6d395a1869b3a4cdcd7188e80bf62cc0194
udsingla <130015308+udsingla@users.noreply.github.com>
2023-05-10 18:51:34 -0700

    grading

--- grade.sh
+++ grade.sh
@@ -1,5 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+set -e
 rm -rf student-submission
 rm -rf grading-area
 
@@ -14,19 +15,32 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
-contains = `false`
+contains= $false
 for file in `find student-submission`
 do
   if [[ -f $file ]] && [[ $file == ListExamples.java ]]
   then
-    contains = true
+    $contains= $true
   fi
 done
 
-if [[$contains == true]]
+if [[ $contains == $true ]]
 then
-  cp ListExamples.java grading-area/
+  cp student-submission/ListExamples.java grading-area/
 else
   echo "File not found."
-  System.exit(1)
+  exit 1
 fi
+
+cp TestListExamples.java grading-area/
+
+set +e
+javac grading-area/ListExamples.java 2> grading-area/errors.txt
+if [[ $? -ne 0 ]]
+then
+  echo compilation error
+  exit 2
+fi
+
+set -e
+echo hello
